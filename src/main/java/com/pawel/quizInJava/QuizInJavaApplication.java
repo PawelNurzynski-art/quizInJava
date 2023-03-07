7ff1284afd3c557199f3130fc92b5ab284b7f8a2
PawelNurzynski-art <you@example.com>
2023-03-07 09:52:57 +0100

    Initial commit

---
+++ src/main/java/com/pawel/quizInJava/QuizInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.pawel.quizInJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizInJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawel/quizInJava/QuizInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawel.quizInJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
