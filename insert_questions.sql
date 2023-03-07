cf6274c0669d2fa3e025bc588bb60fb195684c3d
PawelNurzynski-art <you@example.com>
2023-03-07 10:10:20 +0100

    created insert_questions.sql

---
+++ insert_questions.sql
@@ -0,0 +1,11 @@
+INSERT INTO questions (question_text, option_a, option_b, option_c, option_d, correct_answer) VALUES
+('What is the capital of France?', 'Paris', 'Berlin', 'Madrid', 'London', 'Paris'),
+('What is the highest mountain in the world?', 'Mount Everest', 'K2', 'Kangchenjunga', 'Lhotse', 'Mount Everest'),
+('What is the currency of Japan?', 'Yen', 'Dollar', 'Pound', 'Euro', 'Yen'),
+('Who painted the Mona Lisa?', 'Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Rembrandt', 'Leonardo da Vinci'),
+('What is the largest continent?', 'Asia', 'Africa', 'North America', 'South America', 'Asia'),
+('What is the smallest country in the world?', 'Vatican City', 'Monaco', 'Nauru', 'Tuvalu', 'Vatican City'),
+('What is the chemical symbol for gold?', 'Au', 'Ag', 'Cu', 'Fe', 'Au'),
+('What is the name of the longest river in Africa?', 'Nile', 'Congo', 'Zambezi', 'Niger', 'Nile'),
+('What is the largest planet in our solar system?', 'Jupiter', 'Saturn', 'Neptune', 'Uranus', 'Jupiter'),
+('What is the capital of Australia?', 'Canberra', 'Sydney', 'Melbourne', 'Brisbane', 'Canberra');
